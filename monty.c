8c2e4d73315f6ccbe9bc4cfa48c77f086170e40a
Anteneh <anttesdev>
2023-11-16 02:12:37 -0800

    all

--- monty.c
+++ monty.c
@@ -17,14 +17,14 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 
 	result = process(file, &stack);
